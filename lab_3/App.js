e43456ec786b9911aa5e6c2caac7ecd7f3721043
niniel <natalia.maria.rojas@gmail.com>
2023-06-04 00:40:40 -0600

    Limpieza de codigo
    Se borraron comentarios y los styles se sacaron a otro archivo aparte

--- lab_3/App.js
+++ lab_3/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+import { Text, View, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import { Ionicons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
-
-const screenWidth = Dimensions.get('window').width;
-const photoSize = screenWidth / 3 - 10; // Resta 10 para tener en cuenta los márgenes
+import { styles } from './components/styles';
 
 export default function App() {
   let cameraRef = useRef();
@@ -17,7 +15,6 @@ export default function App() {
   const [photo, setPhoto] = useState();
   const [showCamera, setShowCamera] = useState(false);
   const [photosList, setPhotosList] = useState([]);
-  const [numColumns, setNumColumns] = useState(3);
 
   useEffect(() => {
     (async () => {
@@ -39,20 +36,19 @@ export default function App() {
       alert('Permission to access camera roll is required!');
       return;
     }
-	
+
     const mediaResult = await MediaLibrary.getAssetsAsync({
       mediaType: ['photo'],
       first: 200,
     });
-	console.log(mediaResult.assets);
     setPhotosList(mediaResult.assets);
   };
-  
-	const goBack = () => {
-	  setPhoto(undefined);
-	  setShowCamera(false);
-	  setPhotosList([]);
-	};
+
+  const goBack = () => {
+    setPhoto(undefined);
+    setShowCamera(false);
+    setPhotosList([]);
+  };
 
   let takePic = async () => {
     let options = {
@@ -88,20 +84,20 @@ export default function App() {
       console.log(error);
     }
   };
-  
+
   const toggleCameraType = () => {
-	setCameraType(prevCameraType => {
-    return prevCameraType === Camera.Constants.Type.back
-      ? Camera.Constants.Type.front
-      : Camera.Constants.Type.back;
-	});
+    setCameraType(prevCameraType => {
+      return prevCameraType === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back;
+    });
   };
 
   if (photo) {
     return (
       <SafeAreaView style={styles.container}>
         <Image style={styles.preview} source={{ uri: photo.uri }} />
-		<TouchableOpacity style={styles.goBackButton} onPress={goBack}>
+        <TouchableOpacity style={styles.goBackButton} onPress={goBack}>
           <Ionicons name="arrow-back" size={24} color="#5399C9" />
         </TouchableOpacity>
       </SafeAreaView>
@@ -118,7 +114,7 @@ export default function App() {
           <TouchableOpacity style={styles.goBackButtonCamera} onPress={goBack}>
             <Ionicons name="arrow-back" size={24} color="#5399C9" />
           </TouchableOpacity>
-		  <TouchableOpacity style={styles.toggleCameraButton} onPress={toggleCameraType}>
+          <TouchableOpacity style={styles.toggleCameraButton} onPress={toggleCameraType}>
             <Ionicons name="camera-reverse" size={30} color="#fff" />
           </TouchableOpacity>
         </Camera>
@@ -135,7 +131,7 @@ export default function App() {
             <TouchableOpacity key={item.id} onPress={() => setPhoto(item)}>
               <Image style={styles.thumbnail} source={{ uri: item.uri }} />
             </TouchableOpacity>
-			
+
           ))}
         </ScrollView>
         <TouchableOpacity style={styles.goBackButton} onPress={goBack}>
@@ -147,151 +143,25 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-	<Text style={styles.title}>CAMG2</Text>
-	<View style={styles.infoContainer}>
-  <Text style={styles.infoText}>Laboratorio #3</Text>
-	<Text style={styles.infoTextSub}>Integrantes:</Text>
-      <Text style={styles.infoTextSub}>Natalia Rojas</Text>
-      <Text style={styles.infoTextSub}>Libny Gómez</Text>
-      <Text style={styles.infoTextSub}>Diego Jiménez</Text>
-      <Text style={styles.infoTextSub}>Daniel Gómez</Text>
-    </View>
+      <Text style={styles.title}>CAMG2</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>Laboratorio #3</Text>
+        <Text style={styles.infoTextSub}>Integrantes:</Text>
+        <Text style={styles.infoTextSub}>Natalia Rojas</Text>
+        <Text style={styles.infoTextSub}>Libny Gómez</Text>
+        <Text style={styles.infoTextSub}>Diego Jiménez</Text>
+        <Text style={styles.infoTextSub}>Daniel Gómez</Text>
+      </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.cameraButton} onPress={openCamera}>
           <Ionicons name="camera" size={50} color="#fff" />
-		   <Text style={styles.buttonText}>Cámara</Text>
+          <Text style={styles.buttonText}>Cámara</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.galleryButton} onPress={openGallery}>
-          <Ionicons name="images" size={50} color="#fff"/>
-		  <Text style={styles.buttonText}>Galería</Text>
+          <Ionicons name="images" size={50} color="#fff" />
+          <Text style={styles.buttonText}>Galería</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-	  backgroundColor: '#111A21',
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 60,
-    color:'#138E8E',
-    fontFamily: 'serif',
-  },
-  buttonText: {
-    fontSize: 15,
-    color: '#fff',
-    fontFamily: 'serif',
-  },
-  infoContainer: {
-    width: 330,
-    height: 300,
-    backgroundColor: '#004B5C',
-    borderRadius: 30,
-    padding: 20,
-    marginBottom: 20,
-    },
-  infoText: {
-    fontSize: 18,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontFamily: 'serif',
-    color: '#fff',
-    margin:10
-    },
-  infoTextSub: {
-      fontSize: 16,
-    textAlign: 'center',
-    fontFamily: 'serif',
-    color: '#fff',
-    margin:10
-    },
-  toggleCameraButton: {
-  position: 'absolute',
-  bottom: 30,
-  right: 60,
-  padding: 10,
-},
- buttonCameraContainer: {
-    position: 'absolute',
-    bottom: 20,
-    alignSelf: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '80%',
-  },
-  captureButton: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    borderWidth: 5,
-    borderColor: '#fff',
-  },
-  cameraContainer: {
-    flex: 1,
-    alignSelf: 'stretch',
-  },
-  preview: {
-    flex: 1,
-    alignSelf: 'stretch',
-  },
-  thumbnail: {
-    width: photoSize,
-    height: photoSize,
-    margin: 5,
-  },
-  cameraButton: {
-    width: 150,
-    height: 170,
-    borderRadius: 20,
-    backgroundColor: '#004B5C',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  galleryButton: {
-    width: 150,
-    height: 170,
-    borderRadius: 20,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    backgroundColor: '#004B5C',
-    alignItems: 'center',
-	  marginBottom: 10,
-  },
-  goBackButton: {
-    position: 'absolute',
-    top: 30,
-    left: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 5,
-    zIndex: 1,
-  },
-  goBackButtonCamera: {
-    position: 'absolute',
-    top: 30,
-    left: 20,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 5,
-    zIndex: 1,
-  },
-  photosContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start', // Ajusta aquí para mostrar la última fila en el lado izquierdo
-    marginTop: 70,
-    backgroundColor:'#004B5C',
-    paddingBottom: 70, // Agrega un paddingBottom para mostrar la última fila completa
-  },
-});
